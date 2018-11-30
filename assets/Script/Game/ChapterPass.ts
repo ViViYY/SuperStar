f4d8eac349b7b634f3821951517d095f95ecdf8a
qxw <232385525@qq.com>
2018-11-30 16:22:34 +0800

    fix bug
    fix bug
    
    Signed-off-by: qxw <232385525@qq.com>

--- assets/Script/Game/ChapterPass.ts
+++ assets/Script/Game/ChapterPass.ts
@@ -2,7 +2,7 @@
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class ChapterPass extends cc.Component {
 
     @property(cc.Label)
     chapterLabel: cc.Label = null;
@@ -16,10 +16,27 @@ export default class NewClass extends cc.Component {
 
     load () {
         this.node.setPosition(434, 86);
+        this.hide();
+    }
+
+    private show(): void{
+        cc.log(" ChapterPass show ");
+        this.gameover.opacity = 255;
+        this.chapterLabel.node.opacity = 255;
+        this.targetLabel.node.opacity = 255;
+    }
+
+    private hide(): void{
+        cc.log(" ChapterPass hide ");
+        this.gameover.opacity = 0;
+        this.chapterLabel.node.opacity = 0;
+        this.targetLabel.node.opacity = 0;
     }
 
     public showPass(chapter: number, targetScore: number, cb: Function): void{
         this.gameover.opacity = 0;
+        this.chapterLabel.node.opacity = 255;
+        this.targetLabel.node.opacity = 255;
         this.chapterLabel.string = "关卡：" + chapter.toString();
         this.targetLabel.string = "目标分数：" + targetScore.toString();
         
@@ -28,6 +45,8 @@ export default class NewClass extends cc.Component {
 
     public showOver(cb: Function): void{
         this.gameover.opacity = 255;
+        this.chapterLabel.node.opacity = 0;
+        this.targetLabel.node.opacity = 0;
         this.chapterLabel.string = "";
         this.targetLabel.string = "";
         
@@ -35,9 +54,9 @@ export default class NewClass extends cc.Component {
     }
 
     private showAction(cb: Function): void{
-        this.node.setPosition(434, 86);
         let moveAction: cc.FiniteTimeAction = cc.moveTo(3, cc.v2(-434, 86));
-        let seq: cc.ActionInterval = cc.sequence(moveAction, cc.callFunc(function () {
+        let seq: cc.ActionInterval = cc.sequence(moveAction, cc.callFunc( () => {
+            this.hide();
             if(cb){
                 cb();
             }
